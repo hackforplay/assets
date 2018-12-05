290e431215e7608675e3093f2680eb2c2ab62761
teramotodaiki <i101323@gmail.com>
2018-12-05 16:00:19 +0900

    fix

--- src/assets/_/index.js
+++ src/assets/_/index.js
@@ -9,7 +9,7 @@ module.exports = {
 	scopes: null, // スコープの参照を配列で指定する. null の場合は常に表示
 	insert: null, // 追加ボタン用のコードへのパス. null の場合は追加不可
 	module: './player.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
-	category: cat.キャラクター, // カテゴリーの参照を配列で指定する
+	category: cat.キャラクター, // カテゴリーの参照を指定する
 	icon: './knight-male.png', // アセットのアイコンへのパス
 	production: false, // www.hackforplay.xyz に表示する場合は true. earlybird だけなら false
 	plan: 'free' // 'free' にする
