a5db603abc2e51840ab8081e5d0408e0cf733e69
teramotodaiki <i101323@gmail.com>
2018-12-12 17:17:49 +0900

    fix: テンプレートリテラルの使い方を修正

--- test/index.js
+++ test/index.js
@@ -13,8 +13,8 @@ test('Check categories', t => {
 	for (const key of Object.keys(categories)) {
 		const config = categories[key];
 		const m = name => `${key}の ${name} を指定してください`;
-		t.is(typeof config.name, 'string', m`name`);
-		t.is(typeof config.icon, 'string', m`icon`);
+		t.is(typeof config.name, 'string', m(`name`));
+		t.is(typeof config.icon, 'string', m(`icon`));
 	}
 });
 
@@ -24,10 +24,10 @@ test('Check scopes', t => {
 	for (const key of Object.keys(scopes)) {
 		const config = scopes[key];
 		const m = name => `${key}の ${name} を指定してください`;
-		t.is(typeof config.name, 'string', m`name`);
+		t.is(typeof config.name, 'string', m(`name`));
 		t.truthy(
 			includes(_categories, config.defaultActiveCategory),
-			m`defaultActiveCategory`
+			m(`defaultActiveCategory`)
 		);
 	}
 });
@@ -53,19 +53,19 @@ test('Check configs', t => {
 	// assert recursive
 	function assertAsset(config, domain, abs) {
 		const m = name => `${domain}の ${name} を指定してください`;
-		t.is(typeof config.name, 'string', m`name`);
-		t.is(typeof config.description, 'string', m`description`);
+		t.is(typeof config.name, 'string', m(`name`));
+		t.is(typeof config.description, 'string', m(`description`));
 		if (config.scopes !== null) {
-			t.true(Array.isArray(config.scopes), m`scopes`);
+			t.true(Array.isArray(config.scopes), m(`scopes`));
 			for (const scope of config.scopes) {
 				t.truthy(
 					includes(_scopes, scope),
-					m`scopes にはスコープの配列`
+					m(`scopes にはスコープの配列`)
 				);
 			}
 		}
 		if (config.insert !== null) {
-			t.is(typeof config.insert, 'string', m`insert には文字列`);
+			t.is(typeof config.insert, 'string', m(`insert には文字列`));
 			const filePath = path.join(abs, config.insert);
 			const isExist = fs.existsSync(filePath);
 			t.truthy(
@@ -74,7 +74,7 @@ test('Check configs', t => {
 			);
 		}
 		if (config.module !== null) {
-			t.is(typeof config.module, 'string', m`module には文字列`);
+			t.is(typeof config.module, 'string', m(`module には文字列`));
 			const filePath = path.join(abs, config.module);
 			const isExist = fs.existsSync(filePath);
 			t.truthy(
@@ -86,10 +86,10 @@ test('Check configs', t => {
 			const existFilePath = nameFilePathMap.get(config.name);
 			t.falsy(
 				existFilePath && existFilePath !== filePath,
-				m`${config.name} の module には ${path.relative(
+				`${config.name} の module が統一されていません. ${path.relative(
 					abs,
 					existFilePath + ''
-				)}`
+				)} と ${path.relative(abs, filePath + '')} が存在します`
 			);
 			nameFilePathMap.set(config.name, filePath);
 			const existModuleName = filePathNameMap.get(filePath);
@@ -101,16 +101,16 @@ test('Check configs', t => {
 			);
 			filePathNameMap.set(filePath, config.name);
 		}
-		t.true(includes(_categories, config.category), m`category`);
-		t.is(typeof config.icon, 'string', m`icon には文字列`);
+		t.true(includes(_categories, config.category), m(`category`));
+		t.is(typeof config.icon, 'string', m(`icon には文字列`));
 		const imagePath = path.join(abs, config.icon);
 		const isExist = fs.existsSync(imagePath);
 		t.true(
 			isExist,
 			`${domain}に指定された画像パス ${imagePath} は存在しません`
 		);
-		t.is(typeof config.production, 'boolean', m`production`);
-		t.is(config.plan, 'free', m`plan には 'free'`);
+		t.is(typeof config.production, 'boolean', m(`production`));
+		t.is(config.plan, 'free', m(`plan には 'free'`));
 		if (Array.isArray(config.children)) {
 			for (const child of config.children) {
 				assertAsset(child, domain, abs);
