0ef17b5a720f117d79117545af295d804af19921
RyoTaniguchi <rx.kino.kinoko@gmail.com>
2018-12-30 22:10:13 +0900

    player.js修正

--- src/assets/プレイヤー/player.js
+++ src/assets/プレイヤー/player.js
@@ -6,13 +6,15 @@ rule.つくられたとき(async function() {
 	Player.set(this)
 	this.skin = Hack.skin('ゆうしゃ男') // 見た目をきめる
 	this.family = ('▼ ファミリー', Family.プレイヤー) // ファミリーをきめる
-	this.hp = 3 // 体力をきめる
-	this.atk = 1 // こうげき力をきめる
+	this.n('たいりょく', ('▼ を', 'イコール'), 3) // 体力をきめる
+	this.n('こうげきりょく', ('▼ を', 'イコール'), 1) // こうげき力をきめる
+
 	/*+ つくられたとき */
 })
 
 rule.たおされたとき(async function() {
 	Hack.gameover() // ゲームオーバー
 	this.destroy() // プレイヤーを消す
+
 	/*+ たおされたとき */
 })
