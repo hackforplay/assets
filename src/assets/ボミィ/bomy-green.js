3a2a5e1175493a4e6a88fd108d0a2b0b40d10799
RyoTaniguchi <rx.kino.kinoko@gmail.com>
2019-01-25 13:13:45 +0900

    ボミィ追加

---
+++ src/assets/ボミィ/bomy-green.js
@@ -0,0 +1,61 @@
+import '../game'
+
+rule.this = '緑色のボミィ'
+
+rule.つくられたとき(async function() {
+	await this.costume('緑色のボミィ') // 見た目をきめる
+	this.family = ('▼ なかま', Family.ドクリツ) // なかまをきめる
+	this.n('たいりょく', ('▼ を', 'イコール'), 3) // 体力をきめる
+	this.n('こうげきりょく', ('▼ を', 'イコール'), 1) // こうげき力をきめる
+
+	/*+ つくられたとき */
+})
+
+rule.つねに(async function() {
+	await this.chase4('プレイヤー') // おいかける
+	await this.wait(1) // やすむ
+
+	/*+ つねに */
+})
+
+rule.item = ('▼ あいて', 'プレイヤー')
+rule.ぶつかったとき(async function(item) {
+	this.へんしんする('ばくはつ') // ばくはつ
+	await this.wait(0.7) // まつ
+	rule.つくる('緑色のほうせき', this.mapX, this.mapY) // アイテムをおとす
+
+	/*+ ぶつかったとき */
+})
+
+rule.たおされたとき(async function() {
+	rule.つくる('緑色のほうせき', this.mapX, this.mapY) // アイテムをおとす
+
+	/*+ たおされたとき */
+})
+
+// ここから こうげきされたとき
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.こうげきされたとき(async function(item) {
+	/*+ こうげきされたとき */
+})
+// ここまで こうげきされたとき
+
+// ここから しょうかんされたとき
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.しょうかんされたとき(async function(item) {
+	/*+ しょうかんされたとき */
+})
+// ここまで しょうかんされたとき
+
+// ここから メッセージされたとき
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.メッセージされたとき(async function(item) {
+	/*+ メッセージされたとき */
+})
+// ここまで メッセージされたとき
+
+// ここから すすめなかったとき
+rule.すすめなかったとき(async function() {
+	/*+ すすめなかったとき */
+})
+// ここまで すすめなかったとき
---
+++ src/assets/ボミィ/index.js
@@ -0,0 +1,135 @@
+const sco = require('../../preference/scopes')
+const cat = require('../../preference/categories')
+
+const base = {
+	category: cat.キャラクター, // カテゴリーの参照を配列で指定する
+	description: 'プレイヤーを おいかけて ぶつかったら ばくはつ', // 説明文（日本語）
+	production: false, // www.hackforplay.xyz に表示する場合は true. earlybird だけなら false
+	plan: 'free' // 'free' にする
+}
+
+const black = {
+	...base,
+	name: '黒色のボミィ',
+	module: './bomy-black.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './black_bomy.png' // アセットのアイコンへのパス
+}
+
+const blue = {
+	...base,
+	name: '青色のボミィ',
+	module: './bomy-blue.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './blue_bomy.png' // アセットのアイコンへのパス
+}
+
+const green = {
+	...base,
+	name: '緑色のボミィ',
+	module: './bomy-green.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './green_bomy.png' // アセットのアイコンへのパス
+}
+
+const red = {
+	...base,
+	name: '赤色のボミィ',
+	module: './bomy-red.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './red_bomy.png' // アセットのアイコンへのパス
+}
+
+const white = {
+	...base,
+	name: '白色のボミィ',
+	module: './bomy-white.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './white_bomy.png' // アセットのアイコンへのパス
+}
+
+const yellow = {
+	...base,
+	name: '黄色のボミィ',
+	module: './bomy-yellow.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './yellow_bomy.png' // アセットのアイコンへのパス
+}
+
+const scopeCreate = [
+	// スコープの参照を配列で指定する. null の場合は常に表示
+	sco.ゲームがはじまったとき
+]
+
+const scopeSummon = [
+	// スコープの参照を配列で指定する. null の場合は常に表示
+	sco.こうげきされたとき,
+	sco.たおされたとき,
+	sco.ふまれたとき,
+	sco.ぶつかったとき,
+	sco.メッセージされたとき,
+	sco.みつけたとき
+]
+
+module.exports = [
+	// 「ステージ」ファイルに入るコード
+	{
+		...blue,
+		scopes: scopeCreate,
+		insert: './bomy-blue-create.js', // 追加ボタン用のコードへのパス. null の場合は追加不可
+		children: [
+			{
+				...black,
+				scopes: scopeCreate,
+				insert: './bomy-black-create.js'
+			},
+			{
+				...green,
+				scopes: scopeCreate,
+				insert: './bomy-green-create.js'
+			},
+			{
+				...red,
+				scopes: scopeCreate,
+				insert: './bomy-red-create.js'
+			},
+			{
+				...white,
+				scopes: scopeCreate,
+				insert: './bomy-white-create.js'
+			},
+			{
+				...yellow,
+				scopes: scopeCreate,
+				insert: './bomy-yellow-create.js'
+			}
+		]
+	},
+	// 「◯◯を改造する」ファイルに入るコード
+	{
+		...blue,
+		scopes: scopeSummon,
+		insert: './bomy-blue-summon.js', // 追加ボタン用のコードへのパス. null の場合は追加不可
+		children: [
+			{
+				...black,
+				scopes: scopeSummon,
+				insert: './bomy-black-summon.js'
+			},
+			{
+				...green,
+				scopes: scopeSummon,
+				insert: './bomy-green-summon.js'
+			},
+			{
+				...red,
+				scopes: scopeSummon,
+				insert: './bomy-red-summon.js'
+			},
+			{
+				...white,
+				scopes: scopeSummon,
+				insert: './bomy-white-summon.js'
+			},
+			{
+				...yellow,
+				scopes: scopeSummon,
+				insert: './bomy-yellow-summon.js'
+			}
+		]
+	}
+]
