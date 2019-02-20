a4606f38b8e23048907e915c75953e0dcea46404
teramotodaiki <i101323@gmail.com>
2019-02-20 15:32:18 +0900

    feat: ICategory と IScope を追加

--- src/package.ts
+++ src/package.ts
@@ -2,12 +2,22 @@ import { IOutput } from './output'
 
 export interface IPackage {
 	version: string
-	categories: any[]
-	scopes: any[]
+	categories: ICategory[]
+	scopes: IScope[]
 	module: { [id: string]: IModule }
 	buttons: IOutput[]
 }
 
 export interface IModule {
 	code: string
+}
+
+export interface ICategory {
+	name: string
+	iconUrl: string
+}
+
+export interface IScope {
+	name: string
+	defaultActiveCategory: number
 }
