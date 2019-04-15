bb9edc333ba378715171626bd4da4ed1cb4ddddf
RyoTaniguchi <rx.kino.kinoko@gmail.com>
2019-04-15 13:37:20 +0900

    fix: Hack.logのアセットが出ないように

--- src/assets/ログ/index.js
+++ src/assets/ログ/index.js
@@ -2,19 +2,7 @@ const sco = require('../../preference/scopes')
 const cat = require('../../preference/categories')
 
 const base = {
-	scopes: [
-		sco.ゲームがはじまったとき,
-		sco.たおされたとき,
-		sco.しょうかんされたとき,
-		sco.つくられたとき,
-		sco.ぶつかったとき,
-		sco.こうげきされたとき,
-		sco.つねに,
-		sco.ふまれたとき,
-		sco.メッセージされたとき,
-		sco.みつけたとき,
-		sco.こうげきするとき
-	],
+	scopes: [],
 	module: null, // 改造ボタン用のコードへのパス. null の場合は改造不可
 	category: cat.システム, // カテゴリーの参照を指定する
 	production: false, // www.hackforplay.xyz に表示する場合は true. earlybird だけなら false
