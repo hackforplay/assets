3813c9ef406a2b379dda426325be3c9a9b8bc7ea
RyoTaniguchi <rx.kino.kinoko@gmail.com>
2019-07-06 14:47:57 +0900

    feat: フクロウメイジ追加

---
+++ src/assets/フクロウメイジ/index.js
@@ -0,0 +1,139 @@
+const sco = require('../../preference/scopes')
+const cat = require('../../preference/categories')
+
+const base = {
+	category: cat.キャラクター, // カテゴリーの参照を配列で指定する
+	description: '話しかけると スキルブックをくれる', // 説明文（日本語）
+	production: false, // www.hackforplay.xyz に表示する場合は true. earlybird だけなら false
+	plan: 'free' // 'free' にする
+}
+
+const black = {
+	...base,
+	name: '黒色のフクロウメイジ',
+	module: './owlmage-black.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './owlmage_black.png', // アセットのアイコンへのパス
+	plan: 'paid'
+}
+
+const blue = {
+	...base,
+	name: '青色のフクロウメイジ',
+	module: './owlmage-blue.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './owlmage_blue.png', // アセットのアイコンへのパス
+	plan: 'paid'
+}
+
+const green = {
+	...base,
+	name: '緑色のフクロウメイジ',
+	module: './owlmage-green.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './owlmage_green.png' // アセットのアイコンへのパス
+}
+
+const red = {
+	...base,
+	name: '赤色のフクロウメイジ',
+	module: './owlmage-red.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './owlmage_red.png' // アセットのアイコンへのパス
+}
+
+const white = {
+	...base,
+	name: '白色のフクロウメイジ',
+	module: './owlmage-white.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './owlmage_white.png', // アセットのアイコンへのパス
+	plan: 'paid'
+}
+
+const yellow = {
+	...base,
+	name: '黄色のフクロウメイジ',
+	module: './owlmage-yellow.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './owlmage_yellow.png', // アセットのアイコンへのパス
+	plan: 'paid'
+}
+
+const scopeCreate = [
+	// スコープの参照を配列で指定する. null の場合は常に表示
+	sco.ゲームがはじまったとき
+]
+
+const scopeSummon = [
+	// スコープの参照を配列で指定する. null の場合は常に表示
+	sco.こうげきされたとき,
+	sco.たおされたとき,
+	sco.ふまれたとき,
+	sco.ぶつかったとき,
+	sco.メッセージされたとき,
+	sco.みつけたとき
+]
+
+module.exports = [
+	// 「ステージ」ファイルに入るコード
+	{
+		...green,
+		scopes: scopeCreate,
+		insert: './owlmage-green-create.js', // 追加ボタン用のコードへのパス. null の場合は追加不可
+		children: [
+			{
+				...black,
+				scopes: scopeCreate,
+				insert: './owlmage-black-create.js'
+			},
+			{
+				...blue,
+				scopes: scopeCreate,
+				insert: './owlmage-blue-create.js'
+			},
+			{
+				...red,
+				scopes: scopeCreate,
+				insert: './owlmage-red-create.js'
+			},
+			{
+				...white,
+				scopes: scopeCreate,
+				insert: './owlmage-white-create.js'
+			},
+			{
+				...yellow,
+				scopes: scopeCreate,
+				insert: './owlmage-yellow-create.js'
+			}
+		]
+	},
+	// 「◯◯を改造する」ファイルに入るコード
+	{
+		...green,
+		scopes: scopeSummon,
+		insert: './owlmage-green-summon.js', // 追加ボタン用のコードへのパス. null の場合は追加不可
+		children: [
+			{
+				...black,
+				scopes: scopeSummon,
+				insert: './owlmage-black-summon.js'
+			},
+			{
+				...blue,
+				scopes: scopeSummon,
+				insert: './owlmage-blue-summon.js'
+			},
+			{
+				...red,
+				scopes: scopeSummon,
+				insert: './owlmage-red-summon.js'
+			},
+			{
+				...white,
+				scopes: scopeSummon,
+				insert: './owlmage-white-summon.js'
+			},
+			{
+				...yellow,
+				scopes: scopeSummon,
+				insert: './owlmage-yellow-summon.js'
+			}
+		]
+	}
+]
---
+++ src/assets/フクロウメイジ/owlmage-green.js
@@ -0,0 +1,82 @@
+import '../game'
+
+rule.this = '緑色のフクロウメイジ'
+
+rule.つくられたとき(async function() {
+	await this.costume('緑色のフクロウメイジ') // 見た目をきめる
+	this.family = ('▼ なかま', Family.ドクリツ) // なかまをきめる
+	this.n('たいりょく', ('▼ を', 'イコール'), 10) // 体力をきめる
+	this.n('こうげきりょく', ('▼ を', 'イコール'), 1) // こうげき力をきめる
+	this.showHpLabel = false // HPひょうじを見えないようにする
+
+	/*+ つくられたとき */
+})
+
+rule.item = 'プレイヤー' // ぶつかるキャラクター
+rule.ぶつかったとき(async function(item) {
+	await this.chase4(item) // おいかける
+	await this.talk('ホホ〜ゥ！\nおまえに このスキルブックをやろう')
+	this.しょうかんする('緑色のスキルブック')
+
+	/*+ ぶつかったとき */
+})
+
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.こうげきされたとき(async function(item) {
+	await this.chase4(item) // おいかける
+	this.しょうかんする('緑色のうず').flyToward(item) // itemにむかってとんでいく
+	await this.attack() // こうげきする
+
+	/*+ こうげきされたとき */
+})
+
+// ここから つねに
+rule.つねに(async function() {
+	/*+ つねに */
+})
+// ここまで つねに
+
+// ここから メッセージされたとき
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.メッセージされたとき(async function(item) {
+	/*+ メッセージされたとき */
+})
+// ここまで メッセージされたとき
+
+// ここから じかんがすすんだとき
+rule.じかんがすすんだとき(async function() {
+	/*+ じかんがすすんだとき */
+})
+// ここまで じかんがすすんだとき
+
+// ここから すすめなかったとき
+rule.すすめなかったとき(async function() {
+	/*+ すすめなかったとき */
+})
+// ここまで すすめなかったとき
+
+// ここから みつけたとき
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.みつけたとき(async function(item) {
+	/*+ みつけたとき */
+})
+// ここまで みつけたとき
+
+// ここから しょうかんされたとき
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.しょうかんされたとき(async function(item) {
+	/*+ しょうかんされたとき */
+})
+// ここまで しょうかんされたとき
+
+// ここから こうげきするとき
+rule.こうげきするとき(async function() {
+	/*+ こうげきするとき */
+})
+// ここまで こうげきするとき
+
+// ここから たおされたとき
+rule.たおされたとき(async function() {
+	/*+ たおされたとき */
+})
+// ここまで たおされたとき
