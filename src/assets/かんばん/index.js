bede77d4b087ebc11b254aae273d4af0e8d8b43e
RyoTaniguchi <rx.kino.kinoko@gmail.com>
2019-07-22 15:25:50 +0900

    feat: かんばんのカラーバリエーション追加

--- src/assets/かんばん/index.js
+++ src/assets/かんばん/index.js
@@ -15,6 +15,41 @@ const wood = {
 	icon: './sign_wood.png' // アセットのアイコンへのパス
 }
 
+const black = {
+	...base,
+	name: '黒色のかんばん',
+	module: './sign-black.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './sign_black.png' // アセットのアイコンへのパス
+}
+
+const blue = {
+	...base,
+	name: '青色のかんばん',
+	module: './sign-blue.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './sign_blue.png' // アセットのアイコンへのパス
+}
+
+const green = {
+	...base,
+	name: '緑色のかんばん',
+	module: './sign-green.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './sign_green.png' // アセットのアイコンへのパス
+}
+
+const red = {
+	...base,
+	name: '赤色のかんばん',
+	module: './sign-red.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './sign_red.png' // アセットのアイコンへのパス
+}
+
+const white = {
+	...base,
+	name: '白色のかんばん',
+	module: './sign-white.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './sign_white.png' // アセットのアイコンへのパス
+}
+
 const scopeCreate = [
 	// スコープの参照を配列で指定する. null の場合は常に表示
 	sco.ゲームがはじまったとき
@@ -38,10 +73,60 @@ module.exports = [
 		scopes: scopeCreate,
 		insert: './sign-wood-create.js'
 	},
+	{
+		...black,
+		scopes: scopeCreate,
+		insert: './sign-black-create.js'
+	},
+	{
+		...blue,
+		scopes: scopeCreate,
+		insert: './sign-blue-create.js'
+	},
+	{
+		...green,
+		scopes: scopeCreate,
+		insert: './sign-green-create.js'
+	},
+	{
+		...red,
+		scopes: scopeCreate,
+		insert: './sign-red-create.js'
+	},
+	{
+		...white,
+		scopes: scopeCreate,
+		insert: './sign-white-create.js'
+	},
 	// 「◯◯を改造する」ファイルに入るコード
 	{
 		...wood,
 		scopes: scopeSummon,
 		insert: './sign-wood-summon.js'
+	},
+	{
+		...black,
+		scopes: scopeSummon,
+		insert: './sign-black-summon.js'
+	},
+	{
+		...blue,
+		scopes: scopeSummon,
+		insert: './sign-blue-summon.js'
+	},
+	{
+		...green,
+		scopes: scopeSummon,
+		insert: './sign-green-summon.js'
+	},
+	{
+		...red,
+		scopes: scopeSummon,
+		insert: './sign-red-summon.js'
+	},
+	{
+		...white,
+		scopes: scopeSummon,
+		insert: './sign-white-summon.js'
 	}
 ]
