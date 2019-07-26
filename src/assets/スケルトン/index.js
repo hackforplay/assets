214803e970b133c966a183e0915a7303e8f9e693
RyoTaniguchi <rx.kino.kinoko@gmail.com>
2019-07-26 13:09:35 +0900

    feat: スケルトン追加

---
+++ src/assets/スケルトン/index.js
@@ -0,0 +1,141 @@
+const sco = require('../../preference/scopes')
+const cat = require('../../preference/categories')
+
+const base = {
+	category: cat.キャラクター, // カテゴリーの参照を配列で指定する
+	description: 'プレイヤーをおいかけ、ゆみやでこうげきしてくる', // 説明文（日本語）
+	production: false // www.hackforplay.xyz に表示する場合は true. earlybird だけなら false
+}
+
+const black = {
+	...base,
+	name: '黒色のスケルトン',
+	module: './skeleton-black.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './skeleton_black.png', // アセットのアイコンへのパス
+	plan: 'paid'
+}
+
+const blue = {
+	...base,
+	name: '青色のスケルトン',
+	module: './skeleton-blue.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './skeleton_blue.png', // アセットのアイコンへのパス
+	plan: 'paid'
+}
+
+const green = {
+	...base,
+	name: '緑色のスケルトン',
+	module: './skeleton-green.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './skeleton_green.png', // アセットのアイコンへのパス
+	plan: 'paid'
+}
+
+const red = {
+	...base,
+	name: '赤色のスケルトン',
+	module: './skeleton-red.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './skeleton_red.png', // アセットのアイコンへのパス
+	plan: 'paid'
+}
+
+const white = {
+	...base,
+	name: '白色のスケルトン',
+	module: './skeleton-white.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './skeleton_white.png', // アセットのアイコンへのパス
+	plan: 'free' // 'free' にする
+}
+
+const yellow = {
+	...base,
+	name: '黄色のスケルトン',
+	module: './skeleton-yellow.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './skeleton_yellow.png', // アセットのアイコンへのパス
+	plan: 'paid'
+}
+
+const scopeCreate = [
+	// スコープの参照を配列で指定する. null の場合は常に表示
+	sco.ゲームがはじまったとき
+]
+
+const scopeSummon = [
+	// スコープの参照を配列で指定する. null の場合は常に表示
+	sco.こうげきされたとき,
+	sco.たおされたとき,
+	sco.ふまれたとき,
+	sco.ぶつかったとき,
+	sco.メッセージされたとき,
+	sco.みつけたとき,
+	sco.タップされたとき
+]
+
+module.exports = [
+	// 「ステージ」ファイルに入るコード
+	{
+		...white,
+		scopes: scopeCreate,
+		insert: './skeleton-white-create.js', // 追加ボタン用のコードへのパス. null の場合は追加不可
+		children: [
+			{
+				...black,
+				scopes: scopeCreate,
+				insert: './skeleton-black-create.js'
+			},
+			{
+				...blue,
+				scopes: scopeCreate,
+				insert: './skeleton-blue-create.js'
+			},
+			{
+				...green,
+				scopes: scopeCreate,
+				insert: './skeleton-green-create.js'
+			},
+			{
+				...red,
+				scopes: scopeCreate,
+				insert: './skeleton-red-create.js'
+			},
+			{
+				...yellow,
+				scopes: scopeCreate,
+				insert: './skeleton-yellow-create.js'
+			}
+		]
+	},
+	// 「◯◯を改造する」ファイルに入るコード
+	{
+		...white,
+		scopes: scopeSummon,
+		insert: './skeleton-white-summon.js', // 追加ボタン用のコードへのパス. null の場合は追加不可
+		children: [
+			{
+				...black,
+				scopes: scopeSummon,
+				insert: './skeleton-black-summon.js'
+			},
+			{
+				...blue,
+				scopes: scopeSummon,
+				insert: './skeleton-blue-summon.js'
+			},
+			{
+				...green,
+				scopes: scopeSummon,
+				insert: './skeleton-green-summon.js'
+			},
+			{
+				...red,
+				scopes: scopeSummon,
+				insert: './skeleton-red-summon.js'
+			},
+			{
+				...yellow,
+				scopes: scopeSummon,
+				insert: './skeleton-yellow-summon.js'
+			}
+		]
+	}
+]
---
+++ src/assets/スケルトン/skeleton-blue.js
@@ -0,0 +1,84 @@
+import '../game'
+
+rule.this = '青色のスケルトン'
+
+rule.つくられたとき(async function() {
+	await this.costume('青色のスケルトン') // 見た目をきめる
+	this.family = ('▼ なかま', Family.モンスター) // なかまをきめる
+	this.n('たいりょく', ('▼ を', 'イコール'), 3) // 体力をきめる
+	this.n('こうげきりょく', ('▼ を', 'イコール'), 1) // こうげき力をきめる
+	this.n('みつけられるきょり', ('▼ を', 'イコール'), 10) // みえるきょり
+	this.n('みつけられるはんい', ('▼ を', 'イコール'), 0) // しかいの広さ
+	this.skill = 'や' // スキルをかえる
+
+	/*+ つくられたとき */
+})
+
+rule.つねに(async function() {
+	await this.chase4('プレイヤー') // おいかける
+	await this.find() // さがす
+	await this.wait(2) // まつ
+
+	/*+ つねに */
+})
+
+rule.item = ('▼ あいて', 'プレイヤー')
+rule.みつけたとき(async function(item) {
+	await this.wait(2) // まつ
+	await this.attack() // こうげき
+	await this.find() // さがす
+
+	/*+ みつけたとき */
+})
+
+rule.たおされたとき(async function() {
+	this.おとす('青色のほうせき') // アイテムをおとす
+
+	/*+ たおされたとき */
+})
+
+// ここから こうげきされたとき
+rule.item = ('▼ あいて', 'プレイヤー')
+rule.こうげきされたとき(async function(item) {
+	/*+ こうげきされたとき */
+})
+// ここまで こうげきされたとき
+
+// ここから ぶつかったとき
+rule.item = ('▼ あいて', 'プレイヤー')
+rule.ぶつかったとき(async function(item) {
+	/*+ ぶつかったとき */
+})
+// ここまで ぶつかったとき
+
+// ここから メッセージされたとき
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.メッセージされたとき(async function(item) {
+	/*+ メッセージされたとき */
+})
+// ここまで メッセージされたとき
+
+// ここから じかんがすすんだとき
+rule.じかんがすすんだとき(async function() {
+	/*+ じかんがすすんだとき */
+})
+// ここまで じかんがすすんだとき
+
+// ここから すすめなかったとき
+rule.すすめなかったとき(async function() {
+	/*+ すすめなかったとき */
+})
+// ここまで すすめなかったとき
+
+// ここから しょうかんされたとき
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.しょうかんされたとき(async function(item) {
+	/*+ しょうかんされたとき */
+})
+// ここまで しょうかんされたとき
+
+// ここから こうげきするとき
+rule.こうげきするとき(async function() {
+	/*+ こうげきするとき */
+})
+// ここまで こうげきするとき
