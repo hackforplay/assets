70e4275bc2e696667d9c1b18e423b4958b45a463
teramotodaiki <i101323@gmail.com>
2019-10-03 15:38:32 +0900

    fix: 「れんぞくばくだん」を「つねに」のなかに入れられるように, カテゴリを動きに

--- src/assets/for/for.js
+++ src/assets/for/for.js
@@ -1,4 +1,4 @@
 for (let y = 0; y < 10; y++) {
-	this.locate(this.mapX, y) // テレポート
+	await this.locate(this.mapX, y) // テレポート
 	await this.attack() // ばくだんをおく
 }
--- src/assets/for/index.js
+++ src/assets/for/index.js
@@ -9,16 +9,16 @@ const base = {
 		sco.つくられたとき,
 		sco.ぶつかったとき,
 		sco.こうげきされたとき,
-		// sco.つねに,
+		sco.つねに,
 		sco.ふまれたとき,
 		sco.メッセージされたとき,
 		sco.みつけたとき,
 		sco.こうげきするとき,
-		// sco.じかんがすすんだとき,
+		sco.じかんがすすんだとき,
 		sco.すすめなかったとき
 	],
 	module: null, // 改造ボタン用のコードへのパス. null の場合は改造不可
-	category: cat.システム, // カテゴリーの参照を指定する
+	category: cat.うごき, // カテゴリーの参照を指定する
 	production: false, // www.hackforplay.xyz に表示する場合は true. earlybird だけなら false
 	plan: 'free' // 'free' にする
 }
