620c19b645ba092f96beeb7c9dcd5595f986f27a
RyoTaniguchi <rx.kino.kinoko@gmail.com>
2019-11-15 16:37:27 +0900

    fix: コインの説明文修正

--- src/assets/コイン/index.js
+++ src/assets/コイン/index.js
@@ -5,7 +5,7 @@ const base = {
 	name: 'コイン',
 	module: './coin.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
 	category: cat.もの, // カテゴリーの参照を指定する
-	description: 'お金を とると スコアが ふえる',
+	description: 'とると おかね(money)が ふえる',
 	icon: './coin.png', // アセットのアイコンへのパス
 	production: false, // www.hackforplay.xyz に表示する場合は true. earlybird だけなら false
 	plan: 'free' // 'free' にする
