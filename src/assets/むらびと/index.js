270e2c9d9c106ef282cb5c6a19b68639e4b5ff77
RyoTaniguchi <rx.kino.kinoko@gmail.com>
2020-02-10 12:10:14 +0900

    feat: むらびと男女5種づつ追加

--- src/assets/むらびと/index.js
+++ src/assets/むらびと/index.js
@@ -15,11 +15,91 @@ const alex = {
 	plan: 'free' // 'free' にする
 }
 
+const bob = {
+	...base,
+	name: 'ボブ',
+	module: './villager_bob.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './villager_bob.png', // アセットのアイコンへのパス
+	plan: 'free' // 'free' にする
+}
+
+const charley = {
+	...base,
+	name: 'チャーリー',
+	module: './villager_charley.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './villager_charley.png', // アセットのアイコンへのパス
+	plan: 'free' // 'free' にする
+}
+
+const dan = {
+	...base,
+	name: 'ダン',
+	module: './villager_dan.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './villager_dan.png', // アセットのアイコンへのパス
+	plan: 'free' // 'free' にする
+}
+
+const ed = {
+	...base,
+	name: 'エド',
+	module: './villager_ed.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './villager_ed.png', // アセットのアイコンへのパス
+	plan: 'free' // 'free' にする
+}
+
+const fred = {
+	...base,
+	name: 'フレッド',
+	module: './villager_fred.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './villager_fred.png', // アセットのアイコンへのパス
+	plan: 'free' // 'free' にする
+}
+
 const ally = {
 	...base,
 	name: 'アリー',
 	module: './villager_ally.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
 	icon: './villager_ally.png', // アセットのアイコンへのパス
+	plan: 'free'
+}
+
+const becky = {
+	...base,
+	name: 'ベッキー',
+	module: './villager_becky.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './villager_becky.png', // アセットのアイコンへのパス
+	plan: 'free'
+}
+
+const cathy = {
+	...base,
+	name: 'キャシー',
+	module: './villager_cathy.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './villager_cathy.png', // アセットのアイコンへのパス
+	plan: 'free'
+}
+
+const dolly = {
+	...base,
+	name: 'ドリー',
+	module: './villager_dolly.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './villager_dolly.png', // アセットのアイコンへのパス
+	plan: 'free'
+}
+
+const eliza = {
+	...base,
+	name: 'イライザ',
+	module: './villager_eliza.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './villager_eliza.png', // アセットのアイコンへのパス
+	plan: 'free'
+}
+
+const florrie = {
+	...base,
+	name: 'フローリー',
+	module: './villager_florrie.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './villager_florrie.png', // アセットのアイコンへのパス
 	plan: 'free'
 }
 
@@ -44,22 +124,130 @@ module.exports = [
 	{
 		...alex,
 		scopes: scopeCreate,
-		insert: './villager_alex-create.js' // 追加ボタン用のコードへのパス. null の場合は追加不可
+		insert: './villager_alex-create.js', // 追加ボタン用のコードへのパス. null の場合は追加不可
+		children: [
+			{
+				...bob,
+				scopes: scopeCreate,
+				insert: './villager_bob-create.js'
+			},
+			{
+				...charley,
+				scopes: scopeCreate,
+				insert: './villager_charley-create.js'
+			},
+			{
+				...dan,
+				scopes: scopeCreate,
+				insert: './villager_dan-create.js'
+			},
+			{
+				...ed,
+				scopes: scopeCreate,
+				insert: './villager_ed-create.js'
+			},
+			{
+				...fred,
+				scopes: scopeCreate,
+				insert: './villager_fred-create.js'
+			}
+		]
 	},
 	{
 		...ally,
 		scopes: scopeCreate,
-		insert: './villager_ally-create.js' // 追加ボタン用のコードへのパス. null の場合は追加不可
+		insert: './villager_ally-create.js', // 追加ボタン用のコードへのパス. null の場合は追加不可
+		children: [
+			{
+				...becky,
+				scopes: scopeCreate,
+				insert: './villager_becky-create.js'
+			},
+			{
+				...cathy,
+				scopes: scopeCreate,
+				insert: './villager_cathy-create.js'
+			},
+			{
+				...dolly,
+				scopes: scopeCreate,
+				insert: './villager_dolly-create.js'
+			},
+			{
+				...eliza,
+				scopes: scopeCreate,
+				insert: './villager_eliza-create.js'
+			},
+			{
+				...florrie,
+				scopes: scopeCreate,
+				insert: './villager_florrie-create.js'
+			}
+		]
 	},
 	// 「◯◯を改造する」ファイルに入るコード
 	{
 		...alex,
 		scopes: scopeSummon,
-		insert: './villager_alex-summon.js' // 追加ボタン用のコードへのパス. null の場合は追加不可
+		insert: './villager_alex-summon.js', // 追加ボタン用のコードへのパス. null の場合は追加不可
+		children: [
+			{
+				...bob,
+				scopes: scopeSummon,
+				insert: './villager_bob-summon.js'
+			},
+			{
+				...charley,
+				scopes: scopeSummon,
+				insert: './villager_charley-summon.js'
+			},
+			{
+				...dan,
+				scopes: scopeSummon,
+				insert: './villager_dan-summon.js'
+			},
+			{
+				...ed,
+				scopes: scopeSummon,
+				insert: './villager_ed-summon.js'
+			},
+			{
+				...fred,
+				scopes: scopeSummon,
+				insert: './villager_fred-summon.js'
+			}
+		]
 	},
 	{
 		...ally,
 		scopes: scopeSummon,
-		insert: './villager_ally-summon.js' // 追加ボタン用のコードへのパス. null の場合は追加不可
+		insert: './villager_ally-summon.js', // 追加ボタン用のコードへのパス. null の場合は追加不可
+		children: [
+			{
+				...becky,
+				scopes: scopeSummon,
+				insert: './villager_becky-summon.js'
+			},
+			{
+				...cathy,
+				scopes: scopeSummon,
+				insert: './villager_cathy-summon.js'
+			},
+			{
+				...dolly,
+				scopes: scopeSummon,
+				insert: './villager_dolly-summon.js'
+			},
+			{
+				...eliza,
+				scopes: scopeSummon,
+				insert: './villager_eliza-summon.js'
+			},
+			{
+				...florrie,
+				scopes: scopeSummon,
+				insert: './villager_florrie-summon.js'
+			}
+		]
 	}
 ]
---
+++ src/assets/むらびと/villager_dolly.js
@@ -0,0 +1,80 @@
+import '../game'
+
+rule.つくられたとき(async function() {
+	await this.costume('ドリー') // 見た目をきめる
+
+	/*+ つくられたとき */
+})
+
+rule.item = 'プレイヤー' // ぶつかるキャラクター
+rule.ぶつかったとき(async function(item) {
+	await this.talk('こんにちは')
+
+	/*+ ぶつかったとき */
+})
+
+rule.つねに(async function() {
+	await this.wait(3) // まつ
+	await this.turn(('▼ むき', Dir.うしろ)) // ふりかえる
+	await this.walk() // あるく
+
+	/*+ つねに */
+})
+
+// ここから こうげきされたとき
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.こうげきされたとき(async function(item) {
+	/*+ こうげきされたとき */
+})
+// ここまで こうげきされたとき
+
+// ここから メッセージされたとき
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.メッセージされたとき(async function(item) {
+	/*+ メッセージされたとき */
+})
+// ここまで メッセージされたとき
+
+// ここから じかんがすすんだとき
+rule.じかんがすすんだとき(async function() {
+	/*+ じかんがすすんだとき */
+})
+// ここまで じかんがすすんだとき
+
+// ここから すすめなかったとき
+rule.すすめなかったとき(async function() {
+	/*+ すすめなかったとき */
+})
+// ここまで すすめなかったとき
+
+// ここから みつけたとき
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.みつけたとき(async function(item) {
+	/*+ みつけたとき */
+})
+// ここまで みつけたとき
+
+// ここから しょうかんされたとき
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.しょうかんされたとき(async function(item) {
+	/*+ しょうかんされたとき */
+})
+// ここまで しょうかんされたとき
+
+// ここから こうげきするとき
+rule.こうげきするとき(async function() {
+	/*+ こうげきするとき */
+})
+// ここまで こうげきするとき
+
+// ここから たおされたとき
+rule.たおされたとき(async function() {
+	/*+ たおされたとき */
+})
+// ここまで たおされたとき
+
+// ここから タップされたとき
+rule.タップされたとき(async function() {
+	/*+ タップされたとき */
+})
+// ここまで タップされたとき
