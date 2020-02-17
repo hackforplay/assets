1454deee6b2d55e4806c3f62c03f8125c943e770
RyoTaniguchi <rx.kino.kinoko@gmail.com>
2020-02-17 13:02:51 +0900

    feat: トゲのゆか関係追加
    トゲのゆか、トゲのないゆか、たてにならんだトゲ、よこにならんだトゲ

---
+++ src/assets/トゲのゆか/index.js
@@ -0,0 +1,93 @@
+const sco = require('../../preference/scopes')
+const cat = require('../../preference/categories')
+
+const base = {
+	category: cat.もの, // カテゴリーの参照を配列で指定する
+	description: 'ふむとダメージ！とてもいたい', // 説明文（日本語）
+	production: false, // www.hackforplay.xyz に表示する場合は true. earlybird だけなら false
+	plan: 'free' // 'free' にする
+}
+
+const spike_trap_on = {
+	...base,
+	name: 'トゲのゆか',
+	module: './spike_trap_on.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './spike_trap_on.png' // アセットのアイコンへのパス
+}
+
+const spike_trap_off = {
+	...base,
+	name: 'トゲのないゆか',
+	module: './spike_trap_off.js', // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './spike_trap_off.png' // アセットのアイコンへのパス
+}
+
+const spike_traps_vertical = {
+	...base,
+	name: 'たてにならんだトゲ',
+	module: null, // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './spike_traps_vertical.png' // アセットのアイコンへのパス
+}
+
+const spike_traps_horizontal = {
+	...base,
+	name: 'よこにならんだトゲ',
+	module: null, // 改造ボタン用のコードへのパス. null の場合は改造不可
+	icon: './spike_traps_horizontal.png' // アセットのアイコンへのパス
+}
+
+const scopeCreate = [
+	// スコープの参照を配列で指定する. null の場合は常に表示
+	sco.ゲームがはじまったとき
+]
+
+const scopeSummon = [
+	// スコープの参照を配列で指定する. null の場合は常に表示
+	sco.こうげきされたとき,
+	sco.たおされたとき,
+	sco.ふまれたとき,
+	sco.ぶつかったとき,
+	sco.メッセージされたとき,
+	sco.みつけたとき,
+	sco.タップされたとき
+]
+
+module.exports = [
+	// 「ステージ」ファイルに入るコード
+	{
+		...spike_trap_on,
+		scopes: scopeCreate,
+		insert: './spike_trap_on-create.js', // 追加ボタン用のコードへのパス. null の場合は追加不可
+		thumbnail: 'https://i.gyazo.com/49c8219fd187e8d22a249aad4d0c74c1.gif', // サムネイル画像
+		children: [
+			{
+				...spike_trap_off,
+				scopes: scopeCreate,
+				insert: './spike_trap_off-create.js' // 追加ボタン用のコードへのパス. null の場合は追加不可
+			}
+		]
+	},
+	{
+		...spike_traps_vertical,
+		scopes: scopeCreate,
+		insert: './spike_traps_vertical-create.js' // 追加ボタン用のコードへのパス. null の場合は追加不可
+	},
+	{
+		...spike_traps_horizontal,
+		scopes: scopeCreate,
+		insert: './spike_traps_horizontal-create.js' // 追加ボタン用のコードへのパス. null の場合は追加不可
+	},
+	// 「◯◯を改造する」ファイルに入るコード
+	{
+		...spike_trap_on,
+		scopes: scopeSummon,
+		insert: './spike_trap_on-summon.js', // 追加ボタン用のコードへのパス. null の場合は追加不可
+		children: [
+			{
+				...spike_trap_off,
+				scopes: scopeSummon,
+				insert: './spike_trap_off-summon.js' // 追加ボタン用のコードへのパス. null の場合は追加不可
+			}
+		]
+	}
+]
---
+++ src/assets/トゲのゆか/spike_trap_off.js
@@ -0,0 +1,86 @@
+import '../game'
+
+rule.つくられたとき(async function() {
+	await this.costume('トゲのないゆか') // 見た目をかえる
+	this.family = ('▼ なかま', Family.モンスター)
+	this.n('ふれたときのダメージ', ('▼ を', 'イコール'), 0)
+
+	/*+ つくられたとき */
+})
+
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.メッセージされたとき(async function(item) {
+	await this.wait(0.1) // まつ
+	this.へんしんする('トゲのゆか') // へんしんする
+
+	/*+ メッセージされたとき */
+})
+
+rule.item = ('▼ あいて', 'プレイヤー')
+rule.ふまれたとき(async function(item) {
+	/*+ ふまれたとき */
+})
+
+// ここから つねに
+rule.つねに(async function() {
+	/*+ つねに */
+})
+// ここまで つねに
+
+// ここから こうげきされたとき
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.こうげきされたとき(async function(item) {
+	/*+ こうげきされたとき */
+})
+// ここまで こうげきされたとき
+
+// ここから ぶつかったとき
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.ぶつかったとき(async function(item) {
+	/*+ ぶつかったとき */
+})
+// ここまで ぶつかったとき
+
+// ここから じかんがすすんだとき
+rule.じかんがすすんだとき(async function() {
+	/*+ じかんがすすんだとき */
+})
+// ここまで じかんがすすんだとき
+
+// ここから すすめなかったとき
+rule.すすめなかったとき(async function() {
+	/*+ すすめなかったとき */
+})
+// ここまで すすめなかったとき
+
+// ここから みつけたとき
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.みつけたとき(async function(item) {
+	/*+ みつけたとき */
+})
+// ここまで みつけたとき
+
+// ここから しょうかんされたとき
+rule.item = ('▼ あいて', Rule.Anyone)
+rule.しょうかんされたとき(async function(item) {
+	/*+ しょうかんされたとき */
+})
+// ここまで しょうかんされたとき
+
+// ここから こうげきするとき
+rule.こうげきするとき(async function() {
+	/*+ こうげきするとき */
+})
+// ここまで こうげきするとき
+
+// ここから たおされたとき
+rule.たおされたとき(async function() {
+	/*+ たおされたとき */
+})
+// ここまで たおされたとき
+
+// ここから タップされたとき
+rule.タップされたとき(async function() {
+	/*+ タップされたとき */
+})
+// ここまで タップされたとき
