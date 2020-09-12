7522ae27e1b5364661d6214947b60ced5c2f7e19
RyoTaniguchi <rx.kino.kinoko@gmail.com>
2020-09-12 16:38:59 +0900

    feat: ログのアセット追加

---
+++ src/assets/ログ/index.js
@@ -0,0 +1,37 @@
+const sco = require('../../preference/scopes')
+const cat = require('../../preference/categories')
+
+const base = {
+	scopes: [
+		sco.ゲームがはじまったとき,
+		sco.たおされたとき,
+		sco.しょうかんされたとき,
+		sco.つくられたとき,
+		sco.ぶつかったとき,
+		sco.こうげきされたとき,
+		sco.つねに,
+		sco.ふまれたとき,
+		sco.どかれたとき,
+		sco.メッセージされたとき,
+		sco.へんすうがかわったとき,
+		sco.みつけたとき,
+		sco.こうげきするとき,
+		sco.あるいたとき,
+		sco.タップされたとき,
+		sco.マップがかわったとき
+	],
+	module: null, // 改造ボタン用のコードへのパス. null の場合は改造不可
+	category: cat.システム, // カテゴリーの参照を指定する
+	production: false, // www.hackforplay.xyz に表示する場合は true. earlybird だけなら false
+	plan: 'free' // 'free' にする
+}
+
+module.exports = [
+	{
+		...base,
+		name: 'ログ',
+		description: 'ゲームの下に小さなもじがでる',
+		icon: './log.png', // アセットのアイコンへのパス
+		insert: './log.js' // 追加ボタン用のコードへのパス. null の場合は追加不可
+	}
+]
