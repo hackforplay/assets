6aa1f3de07b1faf875fd6fafd7a17598f6f160f7
RyoTaniguchi <rx.kino.kinoko@gmail.com>
2020-10-01 13:30:41 +0900

    fix: たいりょくログのスコープから「ゲームがはじまったとき」を外す

--- src/assets/ログ/index.js
+++ src/assets/ログ/index.js
@@ -2,33 +2,54 @@ const sco = require('../../preference/scopes')
 const cat = require('../../preference/categories')
 
 const base = {
-	scopes: [
-		sco.ゲームがはじまったとき,
-		sco.たおされたとき,
-		sco.しょうかんされたとき,
-		sco.つくられたとき,
-		sco.ぶつかったとき,
-		sco.こうげきされたとき,
-		sco.つねに,
-		sco.ふまれたとき,
-		sco.どかれたとき,
-		sco.メッセージされたとき,
-		sco.へんすうがかわったとき,
-		sco.みつけたとき,
-		sco.こうげきするとき,
-		sco.あるいたとき,
-		sco.タップされたとき,
-		sco.マップがかわったとき
-	],
 	module: null, // 改造ボタン用のコードへのパス. null の場合は改造不可
 	category: cat.システム, // カテゴリーの参照を指定する
 	production: false, // www.hackforplay.xyz に表示する場合は true. earlybird だけなら false
 	plan: 'free' // 'free' にする
 }
 
+const scopeLog = [
+	sco.ゲームがはじまったとき,
+	sco.たおされたとき,
+	sco.しょうかんされたとき,
+	sco.つくられたとき,
+	sco.ぶつかったとき,
+	sco.こうげきされたとき,
+	sco.つねに,
+	sco.ふまれたとき,
+	sco.どかれたとき,
+	sco.メッセージされたとき,
+	sco.へんすうがかわったとき,
+	sco.みつけたとき,
+	sco.こうげきするとき,
+	sco.あるいたとき,
+	sco.タップされたとき,
+	sco.マップがかわったとき
+]
+
+const scopeHp = [
+	// sco.ゲームがはじまったとき,
+	sco.たおされたとき,
+	sco.しょうかんされたとき,
+	sco.つくられたとき,
+	sco.ぶつかったとき,
+	sco.こうげきされたとき,
+	sco.つねに,
+	sco.ふまれたとき,
+	sco.どかれたとき,
+	sco.メッセージされたとき,
+	sco.へんすうがかわったとき,
+	sco.みつけたとき,
+	sco.こうげきするとき,
+	sco.あるいたとき,
+	sco.タップされたとき,
+	sco.マップがかわったとき
+]
+
 module.exports = [
 	{
 		...base,
+		scopes: scopeLog,
 		name: 'ログ',
 		description: 'ゲームの下に小さなもじがでる',
 		icon: './log.png', // アセットのアイコンへのパス
@@ -36,6 +57,7 @@ module.exports = [
 	},
 	{
 		...base,
+		scopes: scopeHp,
 		name: 'たいりょくログ',
 		description: 'ゲームの下に小さなもじで たいりょくが でる',
 		icon: './log_hp.png', // アセットのアイコンへのパス
